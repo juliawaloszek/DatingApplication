b4597c1fe41d292db1e2ecd493f84f9208b3ff6e
Julia <juliawaloszek@gmail.com>
2019-04-08 16:56:33 +0200

    Retrieving data from the Database

--- API.DatingApp/Controllers/ValuesController.cs
+++ API.DatingApp/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DatingApp.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.DatingApp.Controllers
@@ -11,18 +12,27 @@ namespace API.DatingApp.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult GetValues()
         {
-            return new string[] { "value1", "value2" };
+            var values = _context.Value.ToList();
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public IActionResult GetValue(int id)
         {
-            return "value";
+            var value = _context.Value.FirstOrDefault(x => x.Id ==id);
+            return Ok(value);
         }
 
         // POST api/values
