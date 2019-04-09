e6729fc02301d61b4c4a5c5f261d5de854e4f153
Julia <juliawaloszek@gmail.com>
2019-04-09 10:24:57 +0200

    Validation in the API

--- API.DatingApp/Dtos/UserForRegisterDto.cs
+++ API.DatingApp/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DatingApp.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 6, ErrorMessage = "You must specify password beetween 6 and 15 caracters")]
         public string Password { get; set; }
     }
 }
