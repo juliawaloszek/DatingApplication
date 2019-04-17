260ecc29c0c7cd6ce372615a00b45429e315e9b9
Julia <juliawaloszek@gmail.com>
2019-04-17 23:03:49 +0200

    Protecting routes with a route guard

--- SPA-DatingApp/src/app/_guards/auth.guard.ts
+++ SPA-DatingApp/src/app/_guards/auth.guard.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { CanActivate } from '@angular/router/src/utils/preactivation';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  {
-  
+export class AuthGuard implements CanActivate {
+  path: ActivatedRouteSnapshot[];
+  route: ActivatedRouteSnapshot;
+
+  constructor(private authService: AuthService,
+              private router: Router,
+              private alertify: AlertifyService) {}
+
+  canActivate(): boolean {
+    if (this.authService.loggedIn()) {
+      return true;
+    }
+
+    this.alertify.error('You shall not pass!');
+    this.router.navigate(['/home']);
+    return false;
+  }
 }
--- SPA-DatingApp/src/app/app.module.ts
+++ SPA-DatingApp/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MemberListComponent } from './member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
 
 
 @NgModule({
@@ -41,7 +42,8 @@ import { appRoutes } from './routes';
    providers: [
       AuthService,
       ErrorInterceptorProvider,
-      AlertifyService
+      AlertifyService,
+      AuthGuard
    ],
    bootstrap: [
       AppComponent
