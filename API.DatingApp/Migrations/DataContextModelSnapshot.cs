1a1e637aa0dc2cf137304cd21a47da1a619665f7
Julia <juliawaloszek@gmail.com>
2019-04-17 23:29:42 +0200

    Exploring Entity Framework migrations

--- API.DatingApp/Migrations/DataContextModelSnapshot.cs
+++ API.DatingApp/Migrations/DataContextModelSnapshot.cs
@@ -16,52 +16,10 @@ namespace API.DatingApp.Migrations
             modelBuilder
                 .HasAnnotation("ProductVersion", "2.2.3-servicing-35854");
 
-            modelBuilder.Entity("API.DatingApp.Models.Photo", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<DateTime>("DateAdded");
-
-                    b.Property<string>("Description");
-
-                    b.Property<bool>("IsMain");
-
-                    b.Property<string>("Url");
-
-                    b.Property<int?>("UserId");
-
-                    b.HasKey("Id");
-
-                    b.HasIndex("UserId");
-
-                    b.ToTable("Photos");
-                });
-
             modelBuilder.Entity("API.DatingApp.Models.User", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd();
-
-                    b.Property<string>("City");
-
-                    b.Property<string>("Country");
-
-                    b.Property<DateTime>("Created");
-
-                    b.Property<DateTime>("DateOfBirth");
-
-                    b.Property<string>("Gender");
-
-                    b.Property<string>("Interests");
-
-                    b.Property<string>("Introduction");
-
-                    b.Property<string>("KnownAs");
-
-                    b.Property<DateTime>("LastActive");
-
-                    b.Property<string>("LookingFor");
 
                     b.Property<byte[]>("PasswordHash");
 
@@ -84,13 +42,6 @@ namespace API.DatingApp.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Value");
-                });
-
-            modelBuilder.Entity("API.DatingApp.Models.Photo", b =>
-                {
-                    b.HasOne("API.DatingApp.Models.User")
-                        .WithMany("Photos")
-                        .HasForeignKey("UserId");
                 });
 #pragma warning restore 612, 618
         }
