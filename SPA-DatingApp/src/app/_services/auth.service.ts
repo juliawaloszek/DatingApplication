827c36820bf75737b883e596624314ded86267fe
Julia <juliawaloszek@gmail.com>
2019-04-17 21:49:31 +0200

    Using the angular JWT to improve token handling

--- SPA-DatingApp/src/app/_services/auth.service.ts
+++ SPA-DatingApp/src/app/_services/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
 
 constructor(private http: HttpClient) { }
 
@@ -27,5 +29,9 @@ register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
 }
 
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
+}
 
 }
