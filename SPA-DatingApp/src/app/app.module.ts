5a766f7d5c445c41fdb8799a26f1d05452d7eb6a
Julia <juliawaloszek@gmail.com>
2019-04-17 22:21:21 +0200

    Adding Ngx Bootstrap to power bootstrap components

--- SPA-DatingApp/src/app/app.module.ts
+++ SPA-DatingApp/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -21,6 +22,7 @@ import { AlertifyService } from './_services/alertify.service';
       RegisterComponent
    ],
    imports: [
+      BsDropdownModule.forRoot(),
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
