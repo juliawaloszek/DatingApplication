667d546baf9635c341f1a21e648a3897bb0345e0
Julia <juliawaloszek@gmail.com>
2019-04-28 18:04:25 +0200

    Using AutoMapper

--- API.DatingApp/Controllers/UsersController.cs
+++ API.DatingApp/Controllers/UsersController.cs
@@ -1,6 +1,9 @@
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.DatingApp.Data;
+using API.DatingApp.Dtos;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,23 +15,32 @@ namespace API.DatingApp.Controllers
     public class UsersController : ControllerBase
     {
         private readonly IDatingRepository _repo;
-      public UsersController(IDatingRepository repo)
+        private readonly IMapper _mapper;
+
+        public UsersController(IDatingRepository repo, IMapper mapper)
       {
-          _repo = repo;
-      }  
+            _repo = repo;
+            _mapper = mapper;
+        }  
 
       [HttpGet]
       public async Task<IActionResult> GetUsers()
       {
-          var users = await _repo.GetUsers();
-          return Ok(users);
+           var users = await _repo.GetUsers();
+
+           var usersToReturn = _mapper.Map<IEnumerable <UserForListDto>>(users);
+
+           return Ok(usersToReturn);
       }
 
       [HttpGet("{id}")]
       public async Task<IActionResult> GetUser(int id)
       {
           var user = await _repo.GetUser(id);
-          return Ok(user);
+
+          var userToReturn = _mapper.Map<UserForDetailedDto>(user);
+
+          return Ok(userToReturn);
       }
     }
 }
