c3c653463cb383462a9f7589b75ee3789a79b928
Julia <juliawaloszek@gmail.com>
2019-05-04 21:19:11 +0200

    Creating another angular service

--- SPA-DatingApp/src/app/app.module.ts
+++ SPA-DatingApp/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
+import { UserService } from './_services/user.service';
 
 
 @NgModule({
@@ -43,7 +44,8 @@ import { AuthGuard } from './_guards/auth.guard';
       AuthService,
       ErrorInterceptorProvider,
       AlertifyService,
-      AuthGuard
+      AuthGuard,
+      UserService
    ],
    bootstrap: [
       AppComponent
