47b12376144964adddf65133ab7e45d9278e105d
Julia <juliawaloszek@gmail.com>
2019-05-05 13:10:09 +0200

    Adding a photo gallery to our application

--- SPA-DatingApp/src/app/app.module.ts
+++ SPA-DatingApp/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -59,7 +60,7 @@ export function tokenGetter() {
       AppRoutingModule,
       HttpClientModule,
       FormsModule,
-
+      NgxGalleryModule
    ],
    providers: [
       AuthService,
