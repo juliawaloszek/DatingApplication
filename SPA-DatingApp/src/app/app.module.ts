2b4d529abf4c58d4ac700d4a89c384af4183413e
Julia <juliawaloszek@gmail.com>
2019-05-05 11:25:55 +0200

    Creating the member detailed view component class

--- SPA-DatingApp/src/app/app.module.ts
+++ SPA-DatingApp/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 
 export function tokenGetter() {
@@ -37,7 +38,8 @@ export function tokenGetter() {
       MemberListComponent,
       ListsComponent,
       MessagesComponent,
-      MemberCardComponent
+      MemberCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BsDropdownModule.forRoot(),
