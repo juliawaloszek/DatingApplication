d47a924dfdf31767e1523e9fe214fcf7a6d88479
Julia <juliawaloszek@gmail.com>
2019-05-06 17:44:33 +0200

    Handling Dates in Forms

--- SPA-DatingApp/src/app/app.module.ts
+++ SPA-DatingApp/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule, BsDatepickerModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule } from 'ngx-gallery';
@@ -31,6 +31,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+
 
 
 
@@ -64,6 +65,7 @@ export function tokenGetter() {
          }
       }),
       TabsModule.forRoot(),
+      BsDatepickerModule.forRoot(),
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
