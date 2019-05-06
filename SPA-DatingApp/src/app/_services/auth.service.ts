7eb229c88043e694a78f6fcf9728185ca6b04dbe
Julia <juliawaloszek@gmail.com>
2019-05-06 15:19:54 +0200

    BehaviorSubject to add any to any communication

--- SPA-DatingApp/src/app/_services/auth.service.ts
+++ SPA-DatingApp/src/app/_services/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators';
-import {JwtHelperService} from '@auth0/angular-jwt';
+import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
@@ -13,8 +14,14 @@ export class AuthService {
   jwtHelper = new JwtHelperService();
   decodedToken: any;
   currentUser: User;
+  photoUrl = new BehaviorSubject<string>('../../assets/user.png');
+  currentPhotoUrl = this.photoUrl.asObservable();
 
 constructor(private http: HttpClient) { }
+
+changeMemberPhoto(photoUrl: string) {
+  this.photoUrl.next(photoUrl);
+}
 
 login(model: any) {
   return this.http.post(this.baseUrl + 'login', model)
@@ -27,7 +34,7 @@ login(model: any) {
           localStorage.setItem('user', JSON.stringify(user.user));
           this.decodedToken = this.jwtHelper.decodeToken(user.token);
           this.currentUser = user.user;
-          console.log(this.decodedToken);
+          this.changeMemberPhoto(this.currentUser.photoUrl);
         }
       })
     );
--- SPA-DatingApp/src/app/nav/nav.component.ts
+++ SPA-DatingApp/src/app/nav/nav.component.ts
@@ -10,12 +10,14 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
   model: any = {};
+  photoUrl: string;
 
   constructor(public authService: AuthService,
               private alertify: AlertifyService,
               private router: Router) { }
 
   ngOnInit() {
+    this.authService.currentPhotoUrl.subscribe(photoUrl => this.photoUrl = photoUrl);
   }
 
   login() {
@@ -40,7 +42,7 @@ export class NavComponent implements OnInit {
 
     this.authService.decodedToken = null;
     this.authService.currentUser = null;
-    
+
     this.alertify.message('logged out');
     this.router.navigate(['/home']);
   }
