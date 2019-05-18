41399bcf8fc224f750de57950d7d5dd8604ec072
Julia <juliawaloszek@gmail.com>
2019-05-18 17:28:01 +0200

    Completing the Registration implementation

--- SPA-DatingApp/src/app/_services/auth.service.ts
+++ SPA-DatingApp/src/app/_services/auth.service.ts
@@ -40,8 +40,9 @@ login(model: any) {
     );
 }
 
-register(model: any) {
-  return this.http.post(this.baseUrl + 'register', model);
+register(user: User) {
+  // return this.http.post(this.baseUrl + 'register', model);
+  return this.http.post(this.baseUrl + 'register', user);
 }
 
 loggedIn() {
