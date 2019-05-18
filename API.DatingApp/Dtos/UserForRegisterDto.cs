b7970aea728750a487c4ff7125ccac505bdc9fb3
Julia <juliawaloszek@gmail.com>
2019-05-18 17:04:00 +0200

    Updating the Register method in the API

--- API.DatingApp/Dtos/UserForRegisterDto.cs
+++ API.DatingApp/Dtos/UserForRegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace API.DatingApp.Dtos
@@ -10,5 +11,28 @@ namespace API.DatingApp.Dtos
         [Required]
         [StringLength(15, MinimumLength = 6, ErrorMessage = "You must specify password beetween 6 and 15 caracters")]
         public string Password { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string KnownAs { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
--- API.DatingApp/Helpers/AutoMapperProfiles.cs
+++ API.DatingApp/Helpers/AutoMapperProfiles.cs
@@ -27,6 +27,7 @@ namespace API.DatingApp.Helpers
          CreateMap<UserForUpdateDto, User>();
          CreateMap<Photo, PhotoForReturnDto>();
          CreateMap<PhotoForCreationDto, Photo>();
+         CreateMap<UserForRegisterDto, User>();
      }   
     }
 }
