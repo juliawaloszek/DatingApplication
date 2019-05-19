9633065b101f8e7791049c75cce7d7ea878d9ab8
Julia <juliawaloszek@gmail.com>
2019-05-19 08:21:08 +0200

    Using nix-bootstrap pagination module

--- SPA-DatingApp/src/app/members/member-list/member-list.component.ts
+++ SPA-DatingApp/src/app/members/member-list/member-list.component.ts
@@ -3,6 +3,8 @@ import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { User } from '../../_models/user';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResult } from 'src/app/_models/pagination';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-member-list',
@@ -11,6 +13,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
+  pagination: Pagination;
 
   constructor(private userService: UserService, private alertify: AlertifyService,
               private route: ActivatedRoute) { }
@@ -18,9 +21,14 @@ export class MemberListComponent implements OnInit {
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.users = data.users.result;
+      this.pagination = data.users.pagination;
     });
   }
 
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
 
   // loadUsers() {
   //   this.userService.getUsers().subscribe((users: User[]) => {
@@ -29,4 +37,15 @@ export class MemberListComponent implements OnInit {
   //     this.alertify.error(error);
   //   });
   // }
+
+  loadUsers() {
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+          .subscribe(
+          (res: PaginatedResult<User[]>) => {
+            this.users = res.result;
+            this.pagination = res.pagination;
+          }, error => {
+            this.alertify.error(error);
+          });
+  }
 }
