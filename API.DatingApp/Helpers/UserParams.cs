56cf45b4a61bc1f8736830bc6259f491cdb14dad
Julia <juliawaloszek@gmail.com>
2019-05-19 08:50:26 +0200

    Filtering in the API

--- API.DatingApp/Helpers/UserParams.cs
+++ API.DatingApp/Helpers/UserParams.cs
@@ -10,6 +10,10 @@ namespace API.DatingApp.Helpers
             get { return pageSize; }
             set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
         }
+
+
+        public int UserId { get; set; }
+        public string Gender { get; set; }
         
     }
 }
