4ccce3466b854174cb66dbcd2fc81882c56eda1a
Julia <juliawaloszek@gmail.com>
2019-06-01 10:06:54 +0200

    Creating the Like entity

--- API.DatingApp/Data/DataContext.cs
+++ API.DatingApp/Data/DataContext.cs
@@ -9,6 +9,24 @@ namespace API.DatingApp.Data
         public DbSet<Value> Value {get; set; }
         public DbSet<User> Users {get; set; }
         public DbSet<Photo> Photos { get; set; }
-        
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnModelCreating (ModelBuilder builder)
+        {
+            builder.Entity<Like>()
+                .HasKey(key => new { key.LikerId, key.LikeeId });
+
+            builder.Entity<Like>()
+                .HasOne(user => user.Likee)
+                .WithMany(user => user.Likers)
+                .HasForeignKey(user => user.LikeeId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Like>()
+                .HasOne(user => user.Liker)
+                .WithMany(user => user.Likees)
+                .HasForeignKey(user => user.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
