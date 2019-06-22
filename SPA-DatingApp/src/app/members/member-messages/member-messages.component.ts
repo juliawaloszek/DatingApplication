86803e9f50867e36f12addab509fee1484f9a2a5
Julia <juliawaloszek@gmail.com>
2019-06-22 18:28:20 +0200

    Adding the Mark as Read functionality in SPA

--- SPA-DatingApp/src/app/members/member-messages/member-messages.component.ts
+++ SPA-DatingApp/src/app/members/member-messages/member-messages.component.ts
@@ -4,6 +4,7 @@ import { UserService } from 'src/app/_services/user.service';
 import { AuthService } from 'src/app/_services/auth.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { error } from 'util';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-messages',
@@ -24,7 +25,22 @@ export class MemberMessagesComponent implements OnInit {
 
   // Załadowanie wątku z wiadomościami, całej konwersacji
   loadMessages() {
+    //sztuczka z plusem jeśli zrobimy jak poniżej (dodając +) to cuttertUserId będzie liczbą
+    const currentUserId = +this.authService.decodedToken.nameid;
     this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+    .pipe(
+      // tap = do || Kiedyś nazywało się to "do" jednak to słowo było już zarezerwane
+      // tap wykonaj coś
+      tap(messages => {
+        // przejdziemy przez wszystkie wiadomości szukając tych ktore są nieotworzone przez obecnego użytkownika
+        // tslint:disable-next-line: prefer-for-of
+        for (let i = 0; i < messages.length; i++) {
+          if (messages[i].isRead === false && messages[i].recipientId === currentUserId) {
+              this.userService.markAsRead(currentUserId, messages[i].id);
+          }
+        }
+      })
+    )
     .subscribe(messages => {
       this.messages = messages;
     // tslint:disable-next-line: no-shadowed-variable
