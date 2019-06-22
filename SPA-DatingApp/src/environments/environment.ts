63aeadffaec4ab6a98fec89c83f433b32f89c764
Julia <juliawaloszek@gmail.com>
2019-06-22 20:12:32 +0200

    Using Angular CLI for Ahead of Time Compilation

--- SPA-DatingApp/src/environments/environment.ts
+++ SPA-DatingApp/src/environments/environment.ts
@@ -2,6 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:5000/api/'
+// };
+
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000/api/'
